d0a5835a906a643cf3b8074e6f526c84e3440540
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-05-10 22:53:46 +0800

    关键词提取 iteration 1.1完整输出

--- keywords.py
+++ keywords.py
@@ -1,47 +1,38 @@
 # -*- encoding: utf-8 -*-
 from __future__ import print_function, unicode_literals
 from bosonnlp import BosonNLP
-import xlwings as xw
-import xlrd
+import pandas as pd
+import numpy as np
+import json
+import requests
+import asyncio
 
 nlp = BosonNLP('siOGjeDh.14548.U8dB4nHYKs-x')
-inputPath = r'C:\Main\Desktop\Front End\test\input.xlsx'
-outputPath = r'C:\Main\Desktop\Front End\test\output.xlsx'
+input_path = r'C:\Main\Desktop\Front End\test\input.xlsx'
+output_path = r'C:\Main\Desktop\Front End\test\output.xlsx'
+SENTIMENT_URL = 'http://api.bosonnlp.com/keywords/analysis'
+headers = {'X-Token': 'siOGjeDh.14548.U8dB4nHYKs-x'}
 
-# xlrd模块
-# 计算表格总行数
-mod1Data = xlrd.open_workbook(inputPath)
-mod1Table = mod1Data.sheets()[0]
-mod1Nrows = mod1Table.nrows
-mod1NrowsStr = str(mod1Table.nrows)
-# print(mod1Nrows)
+df_origin = pd.read_excel(input_path, encoding='utf-8')
+raw_data = list(df_origin.iloc[0:5, 0])
+print(raw_data)
 
-# xliwngs模块
-mod2Workbook = xw.Book(inputPath)
-mod2Table = mod2Workbook.sheets[0]
-# 头部
-mod2Table.range('A1').value = 'text'
-mod2Table.range('B1').value = 'weight'
-mod2Table.range('C1').value = 'word'
+async def response():
+	resp = requests.post(SENTIMENT_URL, headers=headers, json=raw_data)
+	resp = resp.text
+	print(resp)
+	resp_array = json.loads(resp)
 
-# 获取rawData范围
-rawData = mod2Table.range('A2:A' + mod1NrowsStr).value
-# 传给波森，并把返回的数据push到数组里
-data = nlp.extract_keywords(rawData)
-print(data)
+	await asyncio.sleep(2)
+	new_data = pd.DataFrame(resp_array)
 
-weight = []
-word = []
-for index, item in enumerate(data):
-	print(index, item)
-	# 为什么可以穿透两层，第三层无法穿透？会自动unpack？
-	for inner1, inner2 in data[index]:
-		weight.append(inner1)
-		word.append(inner2)
+	print(new_data)
 
-print(weight)
-print(word)
-		# mod2Table.range('B' + str(index + 2)).value = weight
-		# mod2Table.range('C' + str(index + 2)).value = word
-
-mod2Workbook.save(outputPath)
+	output = pd.ExcelWriter(output_path)
+	new_data.to_excel(output, 'sheet1')
+	output.save()
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(response())
+loop.close()
