6d72c919b016d68d182afb7ac91fb49f23d6fb5b
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-05-10 10:47:44 +0800

    fix bug，可以正常写入数据

--- ner.py
+++ ner.py
@@ -24,35 +24,19 @@ async def response():
 	resp = requests.post(SENTIMENT_URL, headers=headers, json=raw_data)
 	resp = resp.text
 	print(resp)
+	print(type(json.loads(resp)))
+	resp_array = json.loads(resp)
 	dates = pd.date_range('20170501', periods=5)
 	await asyncio.sleep(2)
-	test1 = [{"tag": ["ns", "n", "n", "nr"],
-	          "word": ["成都", "商报", "记者", "姚永忠"],
-	          "entity": [[0, 2, "product_name"], [2, 3, "job_title"], [3, 4, "person_name"]]
-	          },
-	         {"tag": ["p", "r", "n", "vshi", "d", "vshi", "nr", "ude", "n"],
-	          "word": ["对于", "该", "小孩", "是", "不", "是", "郑尚金", "的", "孩子"],
-	          "entity": [[6, 7, "person_name"]]
-	          },
-	         {"tag": ["t", "d", "v", "n", "n"],
-	          "word": ["目前", "已", "做", "亲子", "鉴定"],
-	          "entity": []
-	          },
-	         {"tag": ["n", "d", "d", "v"],
-	          "word": ["结果", "还", "没", "出来"],
-	          "entity": []
-	          },
-	         {"tag": ["n", "n", "d", "p", "v", "f"],
-	          "word": ["纪检", "部门", "仍", "在", "调查", "之中"],
-	          "entity": []
-	          }]
 	# 不是异步的问题，是不是本地应该写入到一个文件的问题？但不是赋值给了resp吗
 	# 打断点调试
 	# 波森传回来的resp是一个字符串数组'[]'，无语。。。
 	# 想杀了后端的心都有，接口数据定义的这么混乱
-	test2 = pd.DataFrame(resp, index=dates)
+	# 为什么昨天上午用xlwings库时传来的数据能用
+	# json序列化解决
+	new_data = pd.DataFrame(resp_array, index=dates)
 
-	print(test2)
+	print(new_data)
 
 # 获取EventLoop:
 loop = asyncio.get_event_loop()
@@ -61,7 +45,7 @@ loop.run_until_complete(response())
 loop.close()
 
 
-#
+
 # writer = pd.ExcelWriter(output_path)
 # print(writer)
 # df.to_excel(writer, 'sheet1')
